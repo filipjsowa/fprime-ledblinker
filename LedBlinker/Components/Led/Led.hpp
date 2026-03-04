390dcdbb631cbdc3d2d931cf697db18d663a45e8
Filip Sowa <thefil9@gmail.com>
2026-03-04 13:31:54 +0100

    Finished step 3

---
+++ LedBlinker/Components/Led/Led.hpp
@@ -0,0 +1,48 @@
+// ======================================================================
+// \title  Led.hpp
+// \author fsowa
+// \brief  hpp file for Led component implementation class
+// ======================================================================
+
+#ifndef LedBlinker_Led_HPP
+#define LedBlinker_Led_HPP
+
+#include "LedBlinker/Components/Led/LedComponentAc.hpp"
+
+namespace LedBlinker {
+
+class Led final : public LedComponentBase {
+  public:
+    // ----------------------------------------------------------------------
+    // Component construction and destruction
+    // ----------------------------------------------------------------------
+
+    //! Construct Led object
+    Led(const char* const compName  //!< The component name
+    );
+
+    //! Destroy Led object
+    ~Led();
+
+  private:
+    // ----------------------------------------------------------------------
+    // Handler implementations for commands
+    // ----------------------------------------------------------------------
+
+    //! Handler implementation for command BLINKING_ON_OFF
+    //!
+    //! Command to turn on or off the blinking LED
+    void BLINKING_ON_OFF_cmdHandler(FwOpcodeType opCode,  //!< The opcode
+                                    U32 cmdSeq,           //!< The command sequence number
+                                    Fw::On onOff          //!< Indicates whether the blinking should be on or off
+                                    ) override;
+                                    
+    Fw::On m_state = Fw::On::OFF; //! Keeps track if LED is on or off
+    U64 m_transitions = 0; //! The number of on/off transitions that have occurred from FSW boot up
+    U32 m_toggleCounter = 0; //! Keeps track of how many ticks the LED has been on for
+    bool m_blinking = false; //! Flag: if true then LED blinking will occur else no blinking will happen
+};
+
+}  // namespace LedBlinker
+
+#endif
